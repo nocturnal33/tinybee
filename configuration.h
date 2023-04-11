b8b501dd3f10a309debfbb95eec432416d4d8b7a
nocturnal33 <nocturnal33@users.noreply.github.com>
2023-04-11 16:56:33 -0400

    Update configuration.h

--- configuration.h
+++ configuration.h
@@ -1318,7 +1318,7 @@
  *      - normally-closed switches to GND and D32.
  *      - normally-open switches to 5V and D32.
  */
-#define Z_MIN_PROBE_PIN 11 // Pin 32 is the RAMPS default  
+#define Z_MIN_PROBE_PIN IO22 // Pin 32 is the RAMPS default  
 
 /**
  * Probe Type
